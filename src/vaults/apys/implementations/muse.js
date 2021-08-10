f24dd6dcf59c1fe143ae34acf1e260379c5eed87
Bread For The People <bread.for.the.people@planet.earth>
2021-08-10 09:03:35 -0400

    MUSE APY fixed

--- src/vaults/apys/implementations/muse.js
+++ src/vaults/apys/implementations/muse.js
@@ -1,29 +1,26 @@
 const BigNumber = require('bignumber.js')
+const { Cache } = require('../../../lib/db/models/cache')
 const { web3 } = require('../../../lib/web3')
 const { getUIData } = require('../../../lib/data')
-const { getIncentivePoolStats } = require('../../../pools')
 const { getTokenPrice } = require('../../../prices')
-const { pool: poolContractInfo, token: tokenContractData } = require('../../../lib/web3/contracts')
+const { token: tokenContractData } = require('../../../lib/web3/contracts')
 
-const { UI_DATA_FILES } = require('../../../lib/constants')
+const { UI_DATA_FILES, PROFIT_SHARING_POOL_ID, DB_CACHE_IDS } = require('../../../lib/constants')
 const tokenAddresses = require('../../../lib/data/addresses.json')
-const { find } = require('lodash')
+const { find, get } = require('lodash')
 const museMasterContract = require('../../../lib/web3/contracts/muse-masterchef/contract.json')
 const {
   getPoolInfo: getPoolInfoMuse,
 } = require('../../../lib/web3/contracts/muse-masterchef/methods')
+const { cache } = require('../../../lib/cache')
 
 const getApy = async (poolId, lpToken, buybackFraction) => {
   const tokens = await getUIData(UI_DATA_FILES.TOKENS)
-  const pools = await getUIData(UI_DATA_FILES.POOLS)
   const { abi: museMasterAbi, address: museMasterAddress } = museMasterContract
   const {
     methods: { getBalance },
     contract: { abi: tokenAbi },
   } = tokenContractData
-  const {
-    contract: { abi: poolAbi },
-  } = poolContractInfo
 
   const museInstance = new web3.eth.Contract(museMasterAbi, museMasterAddress.mainnet)
   const poolInfo = await getPoolInfoMuse(poolId, museInstance)
@@ -33,9 +30,25 @@ const getApy = async (poolId, lpToken, buybackFraction) => {
     await getBalance(museMasterAddress.mainnet, tokenInstance),
   ).dividedBy(new BigNumber(10).exponentiatedBy(18))
 
+  const dbData = await Cache.find({
+    type: { $in: [DB_CACHE_IDS.STATS, DB_CACHE_IDS.POOLS] },
+  })
+
+  const fetchedStats = dbData.find(result => result.type === DB_CACHE_IDS.STATS)
+  const fetchedPools = dbData.find(result => result.type === DB_CACHE_IDS.POOLS)
+
   let deposit = new BigNumber(1),
     iFARMDeposit = new BigNumber(0),
-    thisDayReward = new BigNumber(0)
+    thisDayReward = new BigNumber(0),
+    iFARMAPR = get(fetchedStats, 'data.tokenStats.historicalAverageProfitSharingAPY', 0)
+
+  if (!iFARMAPR) {
+    iFARMAPR = get(
+      find(get(fetchedPools, 'data.eth', []), pool => pool && pool.id === 'profit-sharing-farm'),
+      'rewardAPY',
+      get(cache.get(`poolRewardApy${PROFIT_SHARING_POOL_ID}`), 'apy', 0),
+    )
+  }
 
   const lpTokenPriceInUSD = await getTokenPrice(tokens[lpToken].tokenAddress)
   const iFARMPriceInUSD = await getTokenPrice(tokenAddresses.iFARM)
@@ -44,22 +57,7 @@ const getApy = async (poolId, lpToken, buybackFraction) => {
   const museReward = new BigNumber(!Number(poolId) ? 250 : 31.25)
   const museRewardInLp = museReward.times(musePriceInUSD).div(lpTokenPriceInUSD)
 
-  const iFARMPool = find(pools, pool => pool.id === 'profit-sharing-farm')
-  const iFARMLPTokenData = { address: tokenAddresses.FARM, decimals: 18 }
-
-  const iFARMAPR = await getIncentivePoolStats(
-    iFARMPool,
-    {
-      ...poolContractInfo,
-      instance: new web3.eth.Contract(poolAbi, iFARMPool.contractAddress),
-    },
-    iFARMLPTokenData,
-    0,
-    undefined,
-    iFARMPool.rewardTokens[0],
-  ).apr
-
-  const dailyPercentageFromiFARM = Math.pow(Number(iFARMAPR / 100) + 1, 1 / 365) - 1
+  const dailyPercentageFromiFARM = Math.pow(Number(iFARMAPR) / 100 + 1, 1 / 365) - 1
   const dailyPercentageFromMuseRewardsInLp = museRewardInLp.div(totalStaked)
 
   for (let day = 0; day < 365; day++) {
@@ -80,6 +78,7 @@ const getApy = async (poolId, lpToken, buybackFraction) => {
   }
 
   const iFARMDepositInUSD = iFARMDeposit.times(iFARMPriceInUSD)
+
   const apy = new BigNumber(iFARMDepositInUSD).dividedBy(lpTokenPriceInUSD).plus(deposit) // convert iFARM back to the units of deposit
 
   return apy.isNaN() ? '0' : apy.times(100).toFixed(2)
