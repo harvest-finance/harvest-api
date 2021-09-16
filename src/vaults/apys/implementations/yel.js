7b5bddb66236282091fe7316c24cf5a2dfae721b
JasperSlimmens <jasper@slimmens.nl>
2021-09-16 10:23:51 +0200

    lint

--- src/vaults/apys/implementations/yel.js
+++ src/vaults/apys/implementations/yel.js
@@ -1,6 +1,5 @@
 const BigNumber = require('bignumber.js')
 const { web3 } = require('../../../lib/web3')
-const getLPTokenPrice = require('../../../prices/implementations/lp-token.js').getPrice
 const tokenAddresses = require('../../../lib/data/addresses.json')
 const yelMasterContract = require('../../../lib/web3/contracts/yel-masterchef/contract.json')
 const {
@@ -9,7 +8,6 @@ const {
   getYelPerSecond,
 } = require('../../../lib/web3/contracts/yel-masterchef/methods')
 
-const { token: tokenContractData } = require('../../../lib/web3/contracts')
 const { getTokenPrice } = require('../../../prices')
 
 const getYelPoolWeight = async (poolInfo, yelInstance) => {
@@ -24,17 +22,12 @@ const getApy = async (poolId, reduction, hodlPoolId, hodlReduction) => {
   } else {
     let baseApr = await getPoolApy(poolId, reduction)
     let hodlApr = await getPoolApy(hodlPoolId, hodlReduction)
-    let hodlApy = ((hodlApr/100/365+1)**365-1)*100
-    return (baseApr*hodlApy/2)/(hodlApr/2)
+    let hodlApy = ((hodlApr / 100 / 365 + 1) ** 365 - 1) * 100
+    return (baseApr * hodlApy) / 2 / (hodlApr / 2)
   }
 }
 
 const getPoolApy = async (poolId, reduction) => {
-  const {
-    methods: { getBalance },
-    contract: { abi },
-  } = tokenContractData
-
   const masterChefContract = yelMasterContract
 
   const yelInstance = new web3.eth.Contract(
@@ -48,8 +41,9 @@ const getPoolApy = async (poolId, reduction) => {
   let secondsPerYear = 31536000
   let poolInfo = await getPoolInfoYel(poolId, yelInstance)
 
-  const tokenInstance = new web3.eth.Contract(abi, poolInfo.stakingToken)
-  const totalSupply = new BigNumber(poolInfo.stakingTokenTotalAmount).dividedBy(new BigNumber(10).exponentiatedBy(18))
+  const totalSupply = new BigNumber(poolInfo.stakingTokenTotalAmount).dividedBy(
+    new BigNumber(10).exponentiatedBy(18),
+  )
   const poolWeight = await getYelPoolWeight(poolInfo, yelInstance)
   const yelPriceInUsd = await getTokenPrice(tokenAddresses.YEL)
   const lpTokenPrice = await getTokenPrice(poolInfo.stakingToken)
