4a45a5d7b1796e21e3809a2259f8eb619e1b00c5
Bread For The People <bread.for.the.people@planet.earth>
2021-10-14 13:17:19 -0400

    Icon fix attempt

--- data/mainnet/pools.js
+++ data/mainnet/pools.js
@@ -4629,7 +4629,7 @@ module.exports = [
     collateralAddress: addresses.V2.sushi_YEL_ETH.NewVault,
     rewardAPY: [],
     rewardTokens: [addresses.iFARM, addresses.V2.yelhold_YEL.NewVault],
-    rewardTokenSymbols: ['iFARM', 'fYEL'],
+    rewardTokenSymbols: ['iFARM', 'YEL'],
     stakeAndDepositHelpMessage: `
       <div class="help-message">
         <b>Deposit and stake:</b>
