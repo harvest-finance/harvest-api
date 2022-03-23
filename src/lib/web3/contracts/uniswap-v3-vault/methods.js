302bfa40bb1e64112e035de807946c724e9c8a09
William <kan881271@gmail.com>
2022-03-22 22:39:18 -0400

    update for univ3 managed vault

---
+++ src/lib/web3/contracts/uniswap-v3-vault/methods.js
@@ -0,0 +1,10 @@
+const { countFunctionCall } = require('../..')
+
+const getCap = instance => countFunctionCall(instance.methods.getCap().call())
+const getDepositCapReached = instance => countFunctionCall(instance.methods.depositCapReached().call())
+const getWithdrawalTimestamp = instance => countFunctionCall(instance.methods.getWithdrawalTimestamp().call())
+const getCurrentCap = instance => countFunctionCall(instance.methods.currentValueInCapToken().call())
+const getPositionIds = instance => countFunctionCall(instance.methods.getPositionIds().call())
+const getCurrentRangePositionId = instance => countFunctionCall(instance.methods.getCurrentRangePositionId().call())
+
+module.exports = { getCap, getDepositCapReached, getWithdrawalTimestamp, getCurrentCap, getPositionIds, getCurrentRangePositionId }
