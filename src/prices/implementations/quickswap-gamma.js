423fbbeba3aa329e9cf8a5dd319233c4f232ea6c
William Liu <william.liu@klinkfinance.com>
2023-02-20 21:27:39 +0000

    lint fix

--- src/prices/implementations/quickswap-gamma.js
+++ src/prices/implementations/quickswap-gamma.js
@@ -4,24 +4,25 @@ const { QUICKSWAP_GAMMA_ENDPOINT } = require('../../lib/constants')
 const BigNumber = require('bignumber.js')
 
 const getPrice = async poolAddress => {
-    let price = 0,
-        response
-    try {
-        response = await axios.get(`${QUICKSWAP_GAMMA_ENDPOINT}hypervisors/allData`)
-        const tvlUSD = get(response, `data.${poolAddress.toLowerCase()}.poolTvlUSD`, 0)
-        let totalSupply = get(response, `data.${poolAddress.toLowerCase()}.totalSupply`, 0)
-        totalSupply = new BigNumber(totalSupply).dividedBy(new BigNumber(10).pow(18))
-        if (totalSupply && totalSupply > 0) {
-            price = parseFloat(tvlUSD) / totalSupply.toFixed()
-        }
-    } catch (e) {
-        console.error('Gamma API error: ', e)
-        price = 0
+  let price = 0,
+    response
+  try {
+    response = await axios.get(`${QUICKSWAP_GAMMA_ENDPOINT}hypervisors/allData`)
+    console.log('response: ', response)
+    const tvlUSD = get(response, `data.${poolAddress.toLowerCase()}.poolTvlUSD`, 0)
+    let totalSupply = get(response, `data.${poolAddress.toLowerCase()}.totalSupply`, 0)
+    totalSupply = new BigNumber(totalSupply).dividedBy(new BigNumber(10).pow(18))
+    if (totalSupply && totalSupply > 0) {
+      price = parseFloat(tvlUSD) / totalSupply.toFixed()
     }
+  } catch (e) {
+    console.error('Gamma API error: ', e)
+    price = 0
+  }
 
-    return price.toString()
+  return price.toString()
 }
 
 module.exports = {
-    getPrice,
+  getPrice,
 }
--- src/vaults/apys/implementations/quickswap-gamma.js
+++ src/vaults/apys/implementations/quickswap-gamma.js
@@ -3,21 +3,24 @@ const { get } = require('lodash')
 const { QUICKSWAP_GAMMA_ENDPOINT } = require('../../../lib/constants')
 
 const getApy = async (masterchef, poolAddress, reduction) => {
-    let response, apy
+  let response, apy
 
-    try {
-        response = await axios.get(`${QUICKSWAP_GAMMA_ENDPOINT}allRewards2`)
-        apy = get(response, `data.${masterchef.toLowerCase()}.pools.${poolAddress.toLowerCase()}.apr`, 0)
-        apy = parseFloat(apy) * parseFloat(reduction) * 100
+  try {
+    response = await axios.get(`${QUICKSWAP_GAMMA_ENDPOINT}allRewards2`)
+    apy = get(
+      response,
+      `data.${masterchef.toLowerCase()}.pools.${poolAddress.toLowerCase()}.apr`,
+      0,
+    )
+    apy = parseFloat(apy) * parseFloat(reduction) * 100
+  } catch (err) {
+    console.error('Gamma API error: ', err)
+    apy = 0
+  }
 
-    } catch (err) {
-        console.error('Gamma API error: ', err)
-        apy = 0
-    }
-
-    return apy
+  return apy
 }
 
 module.exports = {
-    getApy,
+  getApy,
 }
